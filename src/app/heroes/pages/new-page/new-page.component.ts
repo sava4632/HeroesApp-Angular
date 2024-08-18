640b7ea643d15db461798d63e69e915b1a767516
sava4632 <svalencia.arco@gmail.com>
2024-08-18 20:43:17 +0200

    Created reactive form

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -6,9 +9,45 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+
+  public heroForm = new FormGroup({
+    id:               new FormControl<string>(''),
+    superhero:        new FormControl<string>('', { nonNullable: true }),
+    publisher:        new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
+    alt_img:          new FormControl(''),
+  });
   
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' }
   ]
+
+  constructor( private heroesService: HeroesService ){}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSumit(): void {
+
+    if( this.heroForm.invalid ) return;
+    
+    if( this.currentHero.id ){
+      this.heroesService.updateHero( this.currentHero  )
+        .subscribe( hero => {
+          //TODO: mostrar snackbar
+        });
+
+        return;
+    }
+
+    this.heroesService.addHero( this.currentHero  )
+        .subscribe( hero => {
+          //TODO: mostrar snackbar, y navegar a /heores/edit/hero.id
+        });
+  }
 }
